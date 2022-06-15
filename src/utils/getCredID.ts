010d48fa9c87e2d1475cadb78e8de4f2ea3dff88
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-06-15 18:18:25 +0800

    🚧 test

--- src/utils/getCredID.ts
+++ src/utils/getCredID.ts
@@ -10,11 +10,11 @@ const getCredID = async () => {
         requireResidentKey: false,
         userVerification: 'discouraged',
       },
-      challenge: new Uint8Array(16),
+      challenge: new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
       pubKeyCredParams: [],
       rp: { name: 'wmpwd' },
       user: {
-        id: new Uint8Array(16),
+        id: new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
         displayName: 'wmpwd',
         name: "What's my password",
       },
@@ -25,6 +25,7 @@ const getCredID = async () => {
   const uary: Uint8Array = attestation.authData;
   const lenary = uary.slice(53, 55);
   const length = lenary[0] * 255 + lenary[1];
+
   return uAryToB64(uary.slice(55, 55 + length));
 };
 

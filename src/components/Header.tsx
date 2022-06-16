35e4ad91a7c96fe60206519c2cb4f36bc4bab2e5
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-06-16 23:17:51 +0800

    💄

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Header: Component<Props> = (props) => {
   return (
-    <header class="bg-transparent shadow backdrop-filter backdrop-blur-8 backdrop-saturate-50">
+    <header class="bg-transparent shadow backdrop-filter backdrop-blur-8 backdrop-saturate-75">
       <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold leading-tight text-gray-900">{props.title}</h1>
       </div>

37d05c6b3f68d5d7e9d02e8998dd817a8bdd37de
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-06-16 11:40:12 +0800

    💄 Hide main password

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -6,10 +6,17 @@ const Home: Component = () => {
   const [strToHash, setStrToHash] = createSignal('');
   const [mainPassword, setMainPassword] = createSignal('');
   const [hash, setHash] = createSignal('');
+  const [show, setShow] = createSignal(false);
   createEffect(() => {
     setHash('生成中');
     hashForPassword(mainPassword(), strToHash()).then((res) => setHash(res));
   });
+  const showPassword = () => {
+    setShow(true);
+  };
+  const hidePassword = () => {
+    setShow(false);
+  };
   return (
     <>
       <Header title="🔐 我的密码是什么" />
@@ -17,14 +24,26 @@ const Home: Component = () => {
         <div class="flex flex-col mt-4 gap-4 mx-auto max-w-7xl">
           <div class="border-2 border-dashed hover:border-solid border-gray-200 rounded-lg p-4 text-center">
             主密码
-            <input
-              class="border-2 border-dashed focus:border-solid outline-none"
-              style={{ width: '100%' }}
-              onChange={(e) => {
-                setMainPassword(e.currentTarget.value);
-              }}
-              value={mainPassword()}
-            />
+            <label class="flex flex-row gap-1 border-2 border-dashed focus-within:border-solid">
+              <input
+                class="outline-none grow"
+                type={show() ? 'text' : 'password'}
+                onChange={(e) => {
+                  setMainPassword(e.currentTarget.value);
+                }}
+                value={mainPassword()}
+              />
+              <button
+                class="border-l-2 border-dashed px-2"
+                onPointerDown={showPassword}
+                onPointerUp={hidePassword}
+                onPointerLeave={hidePassword}
+                onTouchStart={showPassword}
+                onTouchEnd={hidePassword}
+              >
+                显示
+              </button>
+            </label>
           </div>
           <div class="border-2 border-dashed hover:border-solid border-gray-200 rounded-lg p-4 text-center">
             <div class="flex flex-col gap-2">
